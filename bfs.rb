e5101b1dd2cd1332a9bb6d72db29df25bf34655f
fleuria <me.ssword@gmail.com>
2012-12-09 20:35:55 +0800

    added bfs.rb

---
+++ bfs.rb
@@ -0,0 +1,29 @@
+
+def bfs(g, root)
+  q = [root]
+  while q != []
+    r = q.shift
+    q += (g[r] or [])
+    yield r
+  end
+end
+
+def dfs(g, root)
+  q = [root]
+  while q != []
+    r = q.pop
+    q += (g[r] or []).reverse
+    yield r
+  end
+end
+
+g = {
+  'a' => ['b', 'c'],
+  'b' => ['d', 'e'],
+  'c' => ['f'],
+  'f' => ['g'],
+}
+
+bfs(g, 'a') {|i| puts i }
+puts '---' 
+dfs(g, 'a') {|i| puts i }
